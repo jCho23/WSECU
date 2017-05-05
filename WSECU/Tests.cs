8cc91869b969b75af2371646827186a312737c7c
JuneCho23 <junecho.ucb@gmail.com>
2017-05-04 17:45:31 -0700

    screenshot

--- WSECU/Tests.cs
+++ WSECU/Tests.cs
@@ -49,6 +49,7 @@ namespace WSECU
 		public void PasswordNoID()
 		{
 			app.Tap("password_edit");
+			app.Screenshot("We Tappedon the 'Password' Text Edit Field");
 			app.EnterText("CaroleWashburn");
 			app.DismissKeyboard();
 			app.Tap("signin_button");
